b90792703a0d0f88fa8745ba2c265b06a31ad414
Selena <selena.garciamerino@cieep.com>
2022-10-17 17:39:08 +0200

    Falta update y delete

--- app/src/main/java/com/example/pmdm_04_ejercicioinmobiliaria/MainActivity.java
+++ app/src/main/java/com/example/pmdm_04_ejercicioinmobiliaria/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
 
 
@@ -19,6 +20,8 @@ import com.example.pmdm_04_ejercicioinmobiliaria.databinding.ActivityMainBinding
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -59,13 +62,35 @@ public class MainActivity extends AppCompatActivity {
                                 Piso pisito = (Piso) result.getData().getExtras().getSerializable("PISO");
                                 if(pisito !=null){
                                     pisosLista.add(pisito);
-                                    //que los enseñe con el nuevo view
+                                    mostrarPisos();
                                 }
                             }
                         }
                     }
                 }
         );
+    }
+
+    private void mostrarPisos() {
+
+        binding.contentMain.contenedor.removeAllViews();
+        for (int i = 0; i < pisosLista.size(); i++) {
+            Piso piso = pisosLista.get(i);
+            View inmuebleView = LayoutInflater.from(MainActivity.this).inflate(R.layout.piso_model_view,null);
+            TextView lblDireccion = inmuebleView.findViewById(R.id.lblDireccionPisoView);
+            TextView lblNumero = inmuebleView.findViewById(R.id.lblNumeroAddPiso);
+            TextView lblProvincia = inmuebleView.findViewById(R.id.lblProvinciaPisoModel);
+            TextView lblCiudad = inmuebleView.findViewById(R.id.lblCiudadPisoModel);
+            RatingBar rbValoracion = inmuebleView.findViewById(R.id.rbValPisoModel);
+
+            lblDireccion.setText(piso.getDireccion());
+            lblCiudad.setText(piso.getCiudad());
+            lblProvincia.setText(piso.getProvincia());
+            lblNumero.setText(String.valueOf(piso.getNumero()));
+            rbValoracion.setRating(piso.getValoracion());
+            binding.contentMain.contenedor.addView(inmuebleView);
+        }
+
     }
 
 
